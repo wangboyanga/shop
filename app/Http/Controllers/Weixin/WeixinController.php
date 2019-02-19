8c7822bb26070eac2d27e9ef75942cacd830fb08
wangboyanga <1668540449@qq.com>
2019-02-19 15:15:40 +0800

    ke2

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -82,7 +82,7 @@ class WeixinController extends Controller
     //客服处理
     public function kefu01($openid,$from){
         //文本信息
-        $xml_response='<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$from.']]></FromUserName> <CreateTime>'.time().'</CreateTime> <MsgType>< ![CDATA[text] ]></MsgType> <Content>< ![CDATA['.'Hello World, 现在时间'.date('Y-m-d H:i:s').'] ]></Content> </xml>';
+        $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$from.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. 'Hello World, 现在时间'. date('Y-m-d H:i:s') .']]></Content></xml>';
         echo $xml_response;
     }
 
