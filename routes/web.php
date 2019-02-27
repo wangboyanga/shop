e236c42f3279b27ff272710495d795deef447191
wangboyang <1668540449@qq.com>
2019-02-27 11:21:47 +0800

    tiao

--- routes/web.php
+++ routes/web.php
@@ -123,6 +123,7 @@ Route::post('/weixin/send','Weixin\WeixinController@send');     //客服发给�
 //微信支付
 Route::get('/weixin/pay/test/{order_id}','Weixin\PayController@test');     //微信支付测试
 Route::post('/weixin/pay/notice','Weixin\PayController@notice');     //微信支付通知回调
+Route::post('/weixin/pay/wxsuccess','Weixin\PayController@WxSuccess');     //微信支付通知回调
 
 
 
