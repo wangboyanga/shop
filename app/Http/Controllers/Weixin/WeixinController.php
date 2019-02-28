024bd3e6fa1f7ed8846d8c94d60967c0c8ff963a
wangboyang <1668540449@qq.com>
2019-02-28 14:25:38 +0800

    weixin login

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -480,6 +480,22 @@ class WeixinController extends Controller
         echo '<pre>';print_r($_GET);echo '</pre>';
         $code = $_GET['code'];
         echo 'code: '.$code;
+        $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxe24f70961302b5a5&secret=0f121743ff20a3a454e4a12aeecef4be&code='.$code.'&grant_type=authorization_code';
+        $token_json = file_get_contents($token_url);
+        $token_arr = json_decode($token_json,true);
+        echo '<hr>';
+        echo '<pre>';print_r($token_arr);echo '</pre>';
+
+        $access_token = $token_arr['access_token'];
+        $openid = $token_arr['openid'];
+
+        // 3 携带token  获取用户信息
+        $user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $user_json = file_get_contents($user_info_url);
+
+        $user_arr = json_decode($user_json,true);
+        echo '<hr>';
+        echo '<pre>';print_r($user_arr);echo '</pre>';
     }
 }
 
