0b525fccaaa520488162b8aebab83cb9f25ba6c9
wangboyang <1668540449@qq.com>
2019-03-01 16:42:27 +0800

    wei

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -3,7 +3,8 @@
     <div class="container">
         <h2>JS-SDK</h2>
         <button id="btn1">选择照片</button>
-        <button id="btn2">地理位置</button>
+        <button id="btn2">录音开始</button>
+        <button id="btn3">录音结束</button>
     </div>
 @endsection
 @section("footer")
@@ -17,29 +18,28 @@
             timestamp:"{{$time}}" , // 必填，生成签名的时间戳
             nonceStr: "{{$noncestr}}", // 必填，生成签名的随机串
             signature: "{{$sign}}",// 必填，签名
-            jsApiList: ['checkJsApi','uploadImage','chooseImage','getLocalImgData','startRecord','getLocation'] // 必填，需要使用的JS接口列表
+            jsApiList: ['checkJsApi','uploadImage','chooseImage','getLocalImgData','startRecord','getLocation','startRecord'] // 必填，需要使用的JS接口列表
         });
         wx.ready(function(){
 
 
-            // $("#btn1").click(function () {
-            //     wx.chooseImage({
-            //         count: 9, // 默认9
-            //         sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            //         sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-            //         success: function (res) {
-            //             var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-            //         }
-            //     });
-            // });
-            $('.btn2').click(function () {
-                wx.getLocation({
-                    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+            $("#btn1").click(function () {
+                wx.chooseImage({
+                    count: 9, // 默认9
+                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
                     success: function (res) {
-                        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                        var speed = res.speed; // 速度，以米/每秒计
-                        var accuracy = res.accuracy; // 位置精度
+                        var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    }
+                });
+            });
+            $('.btn2').click(function () {
+                wx.startRecord();
+            })
+            $('.btn3').click(function () {
+                wx.stopRecord({
+                    success: function (res) {
+                        var localId = res.localId;
                     }
                 });
             })
