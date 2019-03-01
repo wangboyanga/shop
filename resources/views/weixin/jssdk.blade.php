dcc2815c80792489a2ddc42a04284fb798334f5f
wangboyang <1668540449@qq.com>
2019-03-01 16:07:58 +0800

    wei

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -33,12 +33,14 @@
                 });
             });
             $('.btn2').click(function () {
-                wx.openLocation({
-                    latitude: 22.545538, // 纬度，浮点数，范围为90 ~ -90。如果是动态获取的数据，使用parseFloat()处理数据
-                    longitude: 114.054565, // 经度，浮点数，范围为180 ~ -180。如果是动态获取的数据，使用parseFloat()处理数据；
-                    name: '老徐家', // 位置名
-                    address: '徐州', // 地址详情说明
-                    scale: 10, // 地图缩放级别,整形值,范围从1~28。默认为最大
+                wx.getLocation({
+                    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+                    success: function (res) {
+                        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                        var speed = res.speed; // 速度，以米/每秒计
+                        var accuracy = res.accuracy; // 位置精度
+                    }
                 });
             })
 
