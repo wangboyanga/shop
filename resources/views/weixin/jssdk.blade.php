bd978968c94091f8b35c345fca482bd3abee619c
wangboyang <1668540449@qq.com>
2019-03-01 16:48:42 +0800

    wei

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -5,6 +5,7 @@
         <button id="btn1">选择照片</button>
         <button id="btn2">录音开始</button>
         <button id="btn3">录音结束</button>
+        <button id="btn4">扫一扫</button>
     </div>
 @endsection
 @section("footer")
@@ -18,7 +19,7 @@
             timestamp:"{{$time}}" , // 必填，生成签名的时间戳
             nonceStr: "{{$noncestr}}", // 必填，生成签名的随机串
             signature: "{{$sign}}",// 必填，签名
-            jsApiList: ['checkJsApi','uploadImage','chooseImage','getLocalImgData','startRecord','getLocation','startRecord'] // 必填，需要使用的JS接口列表
+            jsApiList: ['checkJsApi','uploadImage','chooseImage','getLocalImgData','startRecord','getLocation','stopRecord'] // 必填，需要使用的JS接口列表
         });
         wx.ready(function(){
 
@@ -33,17 +34,28 @@
                     }
                 });
             });
+
             $('.btn2').click(function () {
+                //开始录音
                 wx.startRecord();
             })
             $('.btn3').click(function () {
+                //结束录音
                 wx.stopRecord({
                     success: function (res) {
                         var localId = res.localId;
                     }
                 });
             })
-
+            $('.btn4').click(function () {
+            wx.scanQRCode({
+                needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                success: function (res) {
+                    var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+                }
+            });
+            })
         });
     </script>
 @endsection
